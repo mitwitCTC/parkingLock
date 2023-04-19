75d5621c5b6c0c553104bee3fe7242d9617b3d55
Eileen-Qian <107092198+Eileen-Qian@users.noreply.github.com>
2023-04-19 17:47:50 +0800

    update index
    重構 index；擋板狀態改以圖片呈現

--- js/all.js
+++ js/all.js
@@ -1,70 +1,49 @@
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
-const btn4 = document.getElementById("btn4");
-const btn5 = document.getElementById("btn5");
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
-const status1 = document.getElementById("status1");
-const status2 = document.getElementById("status2");
-const status3 = document.getElementById("status3");
-const status4 = document.getElementById("status4");
-const status5 = document.getElementById("status5");
-
-
-btn1.addEventListener("click", function () {
-    if (btn1.textContent == "車位管制") {
-        status1.textContent = "擋板升起";
-        btn1.textContent = "車位開放";
-        btn1.className = "btn btn-outline-success";
-    } else if (btn1.textContent == "車位開放") {
-        status1.textContent = "擋板降下";
-        btn1.textContent = "車位管制";
-        btn1.className = "btn btn-danger";
+createApp({
+    data() {
+        return {
+            spaces : [
+                {
+                    "id": 1,
+                    "space": "車位一",
+                    "btn": false, // 車位管制
+                    "status": true // 擋板下降
+                },
+                {
+                    "id": 2,
+                    "space": "車位二",
+                    "btn": true,
+                    "status": false
+                },
+                {
+                    "id": 3,
+                    "space": "車位三",
+                    "btn": false,
+                    "status": true
+                },
+                {
+                    "id": 4,
+                    "space": "車位四",
+                    "btn": false,
+                    "status": true
+                },
+                {
+                    "id": 5,
+                    "space": "車位五",
+                    "btn": true,
+                    "status": false
+                }
+            ]
+        }
+    },
+    methods: {
+        toggleBtn(id) {
+            const index = this.spaces.findIndex(space => space.id === id);
+            this.spaces[index].btn = !this.spaces[index].btn;
+            this.spaces[index].status = !this.spaces[index].status;
+        }
+    },
+    mounted(){
     }
-})
-btn2.addEventListener("click", function () {
-    if (btn2.textContent == "車位管制") {
-        status2.textContent = "擋板升起";
-        btn2.textContent = "車位開放";
-        btn2.className = "btn btn-outline-success";
-    } else if (btn2.textContent == "車位開放") {
-        status2.textContent = "擋板降下";
-        btn2.textContent = "車位管制";
-        btn2.className = "btn btn-danger";
-    }
-})
-btn3.addEventListener("click", function () {
-    if (btn3.textContent == "車位管制") {
-        status3.textContent = "擋板升起";
-        btn3.textContent = "車位開放";
-        btn3.className = "btn btn-outline-success";
-    } else if (btn3.textContent == "車位開放") {
-        status3.textContent = "擋板降下";
-        btn3.textContent = "車位管制";
-        btn3.className = "btn btn-danger";
-    }
-})
-btn4.addEventListener("click", function () {
-    if (btn4.textContent == "車位管制") {
-        status4.textContent = "擋板升起";
-        btn4.textContent = "車位開放";
-        btn4.className = "btn btn-outline-success";
-    } else if (btn4.textContent == "車位開放") {
-        status4.textContent = "擋板降下";
-        btn4.textContent = "車位管制";
-        btn4.className = "btn btn-danger";
-    }
-})
-btn5.addEventListener("click", function () {
-    if (btn5.textContent == "車位管制") {
-        status5.textContent = "擋板升起";
-        btn5.textContent = "車位開放";
-        btn5.className = "btn btn-outline-success";
-    } else if (btn5.textContent == "車位開放") {
-        status5.textContent = "擋板降下";
-        btn5.textContent = "車位管制";
-        btn5.className = "btn btn-danger";
-    }
-})
-
-
+}).mount('#app')
